9400ac7a2f4208099bb1d6562cc2fb602be67822
alyilmaz99 <aliyilmaz9977@gmail.com>
2023-05-12 17:43:04 +0300

    first commit

---
+++ whatsapp-auto.py
@@ -0,0 +1,13 @@
+import pyautogui
+import time
+time.sleep(5)
+
+def mesaj():
+    
+        pyautogui.typewrite("text")
+        time.sleep(0)
+        pyautogui.press("enter")
+while True:
+    mesaj()
+  
+  
